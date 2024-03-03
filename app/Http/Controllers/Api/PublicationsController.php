c42fa747e2d33c2f77307c0729500e792228b236
aya <ayahmida64@gmail.com>
2024-03-03 21:25:04 +0100

    storage add

--- app/Http/Controllers/Api/PublicationsController.php
+++ app/Http/Controllers/Api/PublicationsController.php
@@ -44,41 +44,38 @@ class PublicationsController extends Controller
      */
     public function store(Request $request)
     {
-        $validator=Validator::make($request->all(),[
-            'description'=>"required|string|max:200",
-            'file' => 'required|mimes:jpg,png,jpeg,mp4,mov,avi|max:20480',            'user_id'=>"required"
+    $validator = Validator::make($request->all(), [
+        'description' => 'required|string|max:200',
+        'file' => 'required|mimes:jpg,png,jpeg,mp4,mov,avi|max:20480',
+        'user_id' => 'required'
+    ]);
 
-        ]);
-        if ($validator->fails()){
-            $data=[
-                'status'=>422,
-                'message'=>$validator->messages()
-            ];
-            return response()->json($data,422);
+    if ($validator->fails()) {
+        $data = [
+            'status' => 422,
+            'message' => $validator->messages()
+        ];
+        return response()->json($data, 422);
+    } else {
+        if ($request->hasFile('file')) {
+            $file = $request->file('file');
+            $filename = time() . '.' . $file->getClientOriginalExtension();
+            $file->storeAs('uploads', $filename); // Stocke le fichier dans le dossier "uploads" du stockage Laravel
         }
-        else{
 
-            if ($request->has('file')) {
-                $file = $request->file('file');
-                $filename = time().'.'.$file->getClientOriginalExtension();
-                $path='uploads/';
-                $file->move($path,$filename);
-            }
-            $publications=new Publication();
-            $publications->description=$request->description;
-            $publications->file=$filename;
-            $publications->user_id=$request->user_id;
-            $publications->save();
-            $data=[
-                'status'=>200,
-                'message'=>'data create with success'
-            ];
-            return response()->json($data,200);
+        $publication = new Publication();
+        $publication->description = $request->description;
+        $publication->file = $filename;
+        $publication->user_id = $request->user_id;
+        $publication->save();
 
-            
-
-        }
+        $data = [
+            'status' => 200,
+            'message' => 'Data created with success'
+        ];
+        return response()->json($data, 200);
     }
+}
 
     /**
      * Display the specified resource.
